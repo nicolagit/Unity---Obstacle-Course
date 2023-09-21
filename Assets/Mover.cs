b739339ff34c86bc389a5a3dff7b347fa18dcb58
Nicola <nyc.zyp@gmail.com>
2023-09-21 23:17:56 +0100

    1.12 - Introducing Variables

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float xValue = 0.00f;
+    float yValue = 0.01f;
+    float zValue = 0.00f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,7 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
 
     }
 }
