7bd0d852b76a689039600783dc88199e6316b72c
Nicola <nyc.zyp@gmail.com>
2023-10-07 21:53:12 +0100

    2.21 - Using GetComponent<>()

--- Assets/ObjectHit.cs
+++ Assets/ObjectHit.cs
@@ -8,5 +8,6 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Bumped into a wall");
+        GetComponent<MeshRenderer>().material.color = Color.magenta;
     }
 }
