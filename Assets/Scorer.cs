b470f599ca3bfccbfae9e53b260152f4e4cc11a0
Nicola <nyc.zyp@gmail.com>
2023-10-07 22:24:52 +0100

    2.22 - Incrementing A Score (FIX)

--- Assets/Scorer.cs
+++ Assets/Scorer.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 {
-    private int hits = 0;
+    int hits = 0;
 
     private void OnCollisionEnter(Collision other)
     {
         hits++;
-        Debug.Log("You've bumbed into a thing this many times: " + hits);
+        Debug.Log("You've bumped into a thing this many times: " + hits);
     }
 }
